9ac81020d9126ee91f69a2a2d73f19c14ae98c52
Theo <theosayad@Theo.local>
2025-11-25 13:47:55 +0100

    change italian to conversational

--- constants.ts
+++ constants.ts
@@ -140,6 +140,6 @@ export const SKILLS_DATA: SkillItem[] = [
   { name: "English", level: 100, category: "Language" },
   { name: "French", level: 100, category: "Language" },
   { name: "Arabic", level: 100, category: "Language" },
-  { name: "Italian", level: 60, category: "Language" },
+  { name: "Italian", level: 40, category: "Language" },
   { name: "Mandarin", level: 40, category: "Language" },
 ];
